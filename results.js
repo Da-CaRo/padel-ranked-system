7064eb2a6fdda87625e0aecf199767804659618a
Da-CaRo <64038484+Da-CaRo@users.noreply.github.com>
2023-09-09 16:53:00 +0200

    input-structure -  Change quotes

--- results.js
+++ results.js
@@ -1,8 +1,8 @@
 results = [
     {
-        'date': '02/07/2023',
-        'teamA': ['Player1', 'Player2'],
-        'teamB': ['Player3', 'Player4'],
-        'score': ['6','3']
+        "date": "02/07/2023",
+        "teamA": ["Player1", "Player2"],
+        "teamB": ["Player3", "Player4"],
+        "score": ["6","3"]
     }
 ]
